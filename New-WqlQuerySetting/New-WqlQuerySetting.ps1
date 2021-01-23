8d3decc2797988421a8f4d294f4ed25dda01f3cc
cameronkollwitz <cameron@kollwitz.us>
2021-01-23 13:47:46 -0800

    Add New-WqlQuerySetting.ps1

---
+++ New-WqlQuerySetting/New-WqlQuerySetting.ps1
@@ -0,0 +1,65 @@
+﻿Function New-WqlQuerySetting {
+
+  Param (
+    [object]$CI,
+    [string]$WqlQuerySettingClass,
+    [string]$WqlQuerySettingNamspace,
+    [string]$WqlQuerySettingWhere,
+    [string]$WqlQuerySettingProperty
+  )
+
+
+  BEGIN {}
+
+  PROCESS {
+    #Set the WqlQuerySetting object information
+    $WqlQuerySettingDataType = ([Microsoft.ConfigurationManagement.DesiredConfiguration.Rules.ScalarDataType]::Int64)
+    $WqlQuerySettingLogicalName = "WqlSetting_$([Guid]::NewGuid())"
+    $WqlQuerySettingName = "$WqlQuerySettingClass" + '_' + "$WqlQuerySettingProperty"
+    $WqlQuerySettingDescription = 'Scripted setting'
+
+    #Create the WqlQuerySetting object and set the properties
+    $WqlQuerySetting = New-Object -TypeName Microsoft.ConfigurationManagement.DesiredConfiguration.Settings.WqlQuerySetting -ArgumentList $WqlQuerySettingDataType, $WqlQuerySettingLogicalName, $WqlQuerySettingName, $WqlQuerySettingDescription
+    $WqlQuerySetting.Class = $WqlQuerySettingClass
+    $WqlQuerySetting.Namespace = $WqlQuerySettingNamspace
+    $WqlQuerySetting.Where = $WqlQuerySettingWhere
+    $WqlQuerySetting.Property = $WqlQuerySettingProperty
+
+    #Load the SDMPackageXML of the CI
+    $CISDMPackageXML = New-Object -TypeName XML
+    $CISDMPackageXML.LoadXml($($CI.SDMPackageXML))
+
+    #Create the XmlWriterSettings object and set the properties
+    $XMLWriterSettings = New-Object System.Xml.XmlWriterSettings
+    $XMLWriterSettings.Indent = $true
+    $XMLWriterSettings.OmitXmlDeclaration = $false
+    $XMLWriterSettings.NewLineOnAttributes = $true
+
+    #Create the new XmlWriter
+    $XMLWriter = [system.xml.XmlWriter]::Create('C:\Temp\WqlQuerySetting.xml', $XMLWriterSettings)
+
+    #Serialize the WqlQuerySetting object to the XmlWriter
+    $WqlQuerySetting.SerializeToXml($XMLWriter)
+
+    #Flush and dispose the XmlWriter
+    $XMLWriter.Flush()
+    $XMLWriter.Dispose()
+
+    #Load the WqlQuerySettingXML
+    $WqlQuerySettingXML = New-Object -TypeName XML
+    $WqlQuerySettingXML.Load('C:\Temp\WqlQuerySetting.xml')
+
+    #Import the WqlQuerySettingXML node to the SDMPackageXML of the CI
+    $ImportNodeXML = $CISDMPackageXML.ImportNode($WqlQuerySettingXML.SimpleSetting, $true)
+
+    #Add the imported node to the SDMPackageXML of the CI and save it
+    $CISDMPackageXML.DesiredConfigurationDigest.OperatingSystem.Settings.ChildNodes[0].AppendChild($ImportNodeXML)
+    $CISDMPackageXML.Save('C:\Temp\NewSDMPackageXML.xml')
+
+    #Update the CI with the new SDMPackageXML
+    Set-CMConfigurationItem -Name $CI.LocalizedDisplayName -DesiredConfigurationDigestPath C:\Temp\NewSDMPackageXML.xml -Verbose
+
+  }
+
+  END {}
+}
